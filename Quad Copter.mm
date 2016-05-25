abd57154efa99490bdfe80efe8277afe98279088
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-05-25 20:59:06 +0800

    Docs

---
+++ Quad Copter.mm
@@ -0,0 +1,8 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1459003477196" ID="ID_1224306208" MODIFIED="1459003517925" TEXT="Quad Copter">
+<node CREATED="1459003522143" ID="ID_427890264" MODIFIED="1459342355459" POSITION="left" TEXT="Frame">
+<node CREATED="1459003503780" HGAP="38" ID="ID_1869120683" MODIFIED="1459003540388" TEXT="Motors" VSHIFT="-15"/>
+</node>
+</node>
+</map>
